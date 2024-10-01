d4ad61b6a24ad8170744be66488839a832125628
Userator <userator@ya.ru>
2024-10-01 20:35:29 +0300

    add rector package and config

---
+++ rector.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
+use Rector\Php82\Rector\Class_\ReadOnlyClassRector;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/config',
+        __DIR__ . '/public',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->withPhpSets(php82: true)
+    ->withSkip([
+        ReadOnlyClassRector::class,
+        ReadOnlyPropertyRector::class,
+    ]);
